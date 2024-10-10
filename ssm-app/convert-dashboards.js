8ad87cbedb2ae857b7eeabb9efb6e547bf3d87e1
Jason Ng <oblitorum@gmail.com>
2024-10-10 23:54:19 +0800

    Add Health Check dashboard and relevant alert rules

--- ssm-app/convert-dashboards.js
+++ ssm-app/convert-dashboards.js
@@ -3,17 +3,24 @@ const JSON5 = require('json5');
 const path = require('path');
 
 const dashboardDir = 'src/dashboards';
+const alertDir = 'src/alerts';
+
+const parseFunc = (dir, f) => {
+  const filepath = path.join(dir, f);
+  fs.writeFileSync(
+    filepath.replace(/\.json5$/, '.json'),
+    JSON.stringify(
+      JSON5.parse( fs.readFileSync(filepath) ),
+      null,
+      2
+    )
+  );
+};
 
 fs.readdirSync(dashboardDir)
   .filter(f => f.endsWith(".json5"))
-  .forEach(f => {
-    const filepath = path.join(dashboardDir, f);
-    fs.writeFileSync(
-      filepath.replace(/\.json5$/, '.json'),
-      JSON.stringify(
-        JSON5.parse( fs.readFileSync(filepath) ),
-        null,
-        2
-      )
-    );
-  });
+  .forEach(f => parseFunc(dashboardDir, f));
+
+fs.readdirSync(alertDir)
+  .filter(f => f.endsWith(".json5"))
+  .forEach(f => parseFunc(alertDir, f));
